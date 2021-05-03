5cbc42bd0855392ea0246a5c8cb2007f92c4bbc9
Reidzz <83187360+Reidzz@users.noreply.github.com>
2021-05-03 22:31:35 +0100

    Add files via upload

--- roster.js
+++ roster.js
@@ -41,23 +41,22 @@ let rows = 1
 function displayRoster(data, page, pageRows) {
   page--;
 
-  let loopStart = page * pageRows;
-  let paginatedItems = data.slice(loopStart, loopStart + pageRows);
-  for (let i = loopStart; i < loopStart + pageRows; i++) {
-
+  let start = pageRows * page; // 1 x 0 = 0
+  let end = start + pageRows; // 0 + 1 = 1
+  let paginatedItems = data.slice(start, end); // 0, 1 Slice method will select first element in array
+  
+  // For loop that loops through array once
+  // Length method returns value of the array which is 1
+  for (let i = 0; i < paginatedItems.length; i++) {
+    
     let leftCol = `<div class="item">
-                  ${data[i].img}
-                  ${data[i].resident}
+                  ${paginatedItems[i].img}
+                  ${paginatedItems[i].resident}
                   </div>`
                   
-    /*console.log(data[i].img);*/
     // Template literals
 
-    /*let rightCol = `<div class="bio">
-                  ${data[i].bio}
-                  </div>`*/
-
-    let rightCol = data[i].bio
+    let rightCol = paginatedItems[i].bio
     
     rosterArtist.innerHTML = leftCol
     rosterBio.innerHTML = rightCol
@@ -72,14 +71,46 @@ function pagination(data, pageRows, wrapper) {
   wrapper.innerHTML = "";
 
   let pageCount = Math.ceil(data.length / pageRows);
+
   for (let i = 1; i < pageCount + 1; i++) {
     let btn = paginationBtns(i, data);
     wrapper.appendChild(btn);
   }
 
+  let next = document.createElement('button');
+  next.id = "next";
+  let prev = document.createElement('button');
+  prev.id = "prev"
+  let s = document.getElementsByTagName('button')[0]
+  prev.innerText = 'Previous';
+  next.innerText = 'Next';
+  wrapper.insertBefore(prev,s);
+
 }
 
 pagination(rosterItems, rows, rosterPagin);
+
+
+function nextp (page, data, pageRows) {
+  let pageCount = Math.ceil(data.length / pageRows);
+  document.getElementById("next").addEventListener('click', () => {
+    if (parseInt(page.id) === pageCount) {
+    } else {
+      currentPage = parseInt(page.id) + 1;
+       displayRoster(data, currentPage, rows);
+     }
+   });
+}
+
+function prevp (page, data) {
+  document.getElementById("prev").addEventListener('click', ()=> {
+    if (parseInt(page.id) === 1) {
+    } else {
+      currentPage = parseInt(page.id) - 1;
+      displayRoster(data, currentPage, rows);
+     }
+   });
+}
 
 
 function paginationBtns(page, data) {
@@ -87,11 +118,11 @@ function paginationBtns(page, data) {
   let button = document.createElement('button');
   button.innerText = page;
 
-  let next = document.createElement('button');
+  /*let next = document.createElement('button');
   next.innerText = 'Next';
 
   let prev = document.createElement('button');
-  prev.innerText = 'Previous';
+  prev.innerText = 'Previous';*/
 
   if (currentPage == page);
   button.addEventListener('click', function() {
@@ -99,18 +130,17 @@ function paginationBtns(page, data) {
     displayRoster(data, currentPage, rows);
   });
 
-  /*if (currentPage += page);
-  button.addEventListener('click', function() {
-    currentPage = page;
-    displayRoster(data, currentPage, rows);
-  });*/
-
   return button;
 
 }
 
-function nextBtn() {
+/*function nextBtn(page, data) {
 
-}
+  let pageCount = Math.ceil(data.length / pageRows);
+  next.addEventListener('click', function() {
+    if (currentPage += page);
+  });
+
+}*/
 
 
